56cbcb4c810e1e45b4a43e44ca8a650339cee7f0
Ganesh7386 <puttaganesh7386@gmail.com>
2024-06-19 14:25:48 +0530

    passed data from home to news omponent route

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
-import {BrowserRouter} from 'react-router-dom'
-
 import './App.css';
 import OverAllApplication from './components/OverAllApplication/index'
 
 function App() {
   return (
     <div className="App">
-    <BrowserRouter><OverAllApplication/></BrowserRouter>
+      <OverAllApplication/>
     </div>
-   
   );
 }
 
